43e3d2ed573ca309096145db5c238f71ab1e3575
stefano bertozzi <ste.bertozzi@gmail.com>
2018-01-05 11:58:03 +0100

    Aggiunto progetto VM

--- src/WpfAppDiTest.UI/MainWindow.xaml.cs
+++ src/WpfAppDiTest.UI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfAppDiTest.ViewModels;
 
 namespace WpfAppDiTest
 {
@@ -23,6 +24,8 @@ namespace WpfAppDiTest
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = new TitleVM("Questo è il mio titolo");
         }
     }
 }
---
+++ src/WpfAppDiTest.ViewModels/TitleVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppDiTest.ViewModels
+{
+    public class TitleVM
+    {
+        public TitleVM(string title)
+        {
+            Title = title;
+        }
+
+        public string Title { get; }
+    }
+}
